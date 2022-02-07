1cdccc7828aac3a6198865c8764252697de7baa5
Greg Albrecht <github.com@undef.net>
2022-02-07 12:52:24 -0800

    Code cleanup.

--- adsbxcot/classes.py
+++ adsbxcot/classes.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""ADS-B Exchange Cursor-on-Target Class Definitions."""
-
-import concurrent
+"""ADSBXCOT Class Definitions."""
 
 import aiohttp
 import asyncio
 import configparser
-import json
-import logging
-import os
-import queue
-import random
-import threading
-import time
+
 import urllib
 
 import pytak
-import requests
 
 import aircot
 
@@ -26,20 +17,22 @@ import adsbxcot
 
 
 __author__ = "Greg Albrecht W2GMD <oss@undef.net>"
-__copyright__ = "Copyright 2021 Orion Labs, Inc."
+__copyright__ = "Copyright 2022 Greg Albrecht"
 __license__ = "Apache License, Version 2.0"
 
 
 class ADSBXWorker(pytak.MessageWorker):
 
-    """Reads ADS-B Exchange Data, renders to CoT, and puts on queue."""
+    """Reads ADSBExchange.com ADS-B Data, renders to COT, and puts on Queue."""
 
     def __init__(self, event_queue: asyncio.Queue, opts):
         super().__init__(event_queue)
 
-        self.url: urllib.parse.ParseResult = urllib.parse.urlparse(opts.get("ADSBX_URL"))
+        self.url: urllib.parse.ParseResult = urllib.parse.urlparse(
+            opts.get("ADSBX_URL"))
         self.cot_stale = opts.get("COT_STALE")
-        self.poll_interval: int = int(opts.get("POLL_INTERVAL") or adsbxcot.DEFAULT_POLL_INTERVAL)
+        self.poll_interval: int = int(opts.get("POLL_INTERVAL") or
+                                      adsbxcot.DEFAULT_POLL_INTERVAL)
         self.api_key: str = opts.get("API_KEY")
 
         self.include_tisb = bool(opts.get("INCLUDE_TISB")) or False
@@ -52,25 +45,22 @@ class ADSBXWorker(pytak.MessageWorker):
         self.filter_type = ""
         self.known_craft_db = None
 
-
     async def handle_message(self, aircraft: list) -> None:
         """
-        Transforms Aircraft ADS-B data to CoT and puts it onto tx queue.
+        Transforms Aircraft ADS-B data to COT and puts it onto tx queue.
         """
         if not isinstance(aircraft, list):
             self._logger.warning(
                 "Invalid aircraft data, should be a Python list.")
-            return False
+            return None
 
         if not aircraft:
             self._logger.warning("Empty aircraft list")
-            return False
+            return None
 
         _lac = len(aircraft)
         _acn = 1
         for craft in aircraft:
-            # self._logger.debug("craft=%s", craft)
-
             icao = craft.get("hex", craft.get("icao")).strip().upper()
             flight = craft.get("flight", "").strip().upper()
             reg = craft.get("r", "").strip().upper()
@@ -90,24 +80,25 @@ class ADSBXWorker(pytak.MessageWorker):
                 else:
                     filter_key: str = ""
 
-                # self._logger.debug("filter_key=%s", filter_key)
-
                 if self.known_craft_db and filter_key:
                     known_craft = (list(filter(
-                        lambda x: x[self.known_craft_key].strip().upper() == filter_key, self.known_craft_db)) or
+                        lambda x: x[self.known_craft_key].strip().upper() ==
+                                  filter_key, self.known_craft_db)) or
                                    [{}])[0]
                     # self._logger.debug("known_craft='%s'", known_craft)
                 elif filter_key:
-                    if "include" in self.filters[self.filter_type] and filter_key not in self.filters.get(filter_type,
-                                                                                                     "include"):
+                    if "include" in self.filters[self.filter_type] and \
+                            filter_key not in self.filters.get(filter_type,
+                                                               "include"):
                         continue
-                    if "exclude" in self.filters[self.filter_type] and filter_key in self.filters.get(filter_type,
-                                                                                                 "exclude"):
+                    if "exclude" in self.filters[self.filter_type] and \
+                            filter_key in self.filters.get(filter_type,
+                                                           "exclude"):
                         continue
 
-
-            # If we're using a known_craft csv and this craft wasn't found, skip:
-            if self.known_craft_db and not known_craft and not self.include_all_craft:
+            # Skip if we're using known_craft CSV and this Craft isn't found:
+            if self.known_craft_db and not known_craft \
+                    and not self.include_all_craft:
                 continue
 
             event = adsbxcot.adsbx_to_cot(
@@ -132,9 +123,12 @@ class ADSBXWorker(pytak.MessageWorker):
             await self._put_event_queue(event)
             _acn += 1
 
-    async def _get_adsbx_feed(self):
-
-        # Support for either direct ADSBX API, or RapidAPI
+    async def _get_adsbx_feed(self) -> None:
+        """
+        ADSBExchange.com ADS-B Feed API Client wrapper.
+        Connects to ADSBX API and passes messages to `self.handle_message()`.
+        """
+        # Support for either direct ADSBX API, or RapidAPI:
         if "rapidapi" in self.url.geturl():
             headers = {
                 "x-rapidapi-key": self.api_key,
@@ -152,10 +146,16 @@ class ADSBXWorker(pytak.MessageWorker):
             response.raise_for_status()
             json_resp = await response.json()
             aircraft = json_resp.get("ac")
-            self._logger.debug("Retrieved %s aircraft", len(aircraft))
+
+            if aircraft:
+                aircraft_len = len(aircraft)
+            else:
+                aircraft_len = "No"
+            self._logger.debug("Retrieved %s aircraft", aircraft_len)
+
             await self.handle_message(aircraft)
 
-    async def run(self):
+    async def run(self) -> None:
         """Runs this Thread, Reads from Pollers."""
         self._logger.info(
             "Running ADSBXWorker with URL '%s'", self.url.geturl())
@@ -166,7 +166,8 @@ class ADSBXWorker(pytak.MessageWorker):
             self.filters = configparser.ConfigParser()
             self.filters.add_section(self.known_craft_key)
             self.filters[self.known_craft_key]["include"] = \
-                str([x[self.known_craft_key].strip().upper() for x in self.known_craft_db])
+                str([x[self.known_craft_key].strip().upper() for x
+                     in self.known_craft_db])
 
         if self.filters or self.known_craft_db:
             filter_src = self.filters or self.known_craft_key
@@ -183,4 +184,3 @@ class ADSBXWorker(pytak.MessageWorker):
         while 1:
             await self._get_adsbx_feed()
             await asyncio.sleep(self.poll_interval)
-
