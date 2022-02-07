e6fbaefa02ddb7d5546f05d04db47da7f32d7efd
Greg Albrecht <github.com@undef.net>
2022-02-07 12:51:12 -0800

    Updated description.

--- adsbxcot/__init__.py
+++ adsbxcot/__init__.py
@@ -9,7 +9,7 @@ ADS-B Exchange Cursor-on-Target Gateway.
 
 
 :author: Greg Albrecht W2GMD <oss@undef.net>
-:copyright: Copyright 2020 Orion Labs, Inc.
+:copyright: Copyright 2022 Greg Albrecht
 :license: Apache License, Version 2.0
 :source: <https://github.com/ampledata/adsbxcot>
 
@@ -23,5 +23,5 @@ from .functions import adsbx_to_cot  # NOQA
 from .classes import ADSBXWorker  # NOQA
 
 __author__ = "Greg Albrecht W2GMD <oss@undef.net>"
-__copyright__ = "Copyright 2020 Orion Labs, Inc."
+__copyright__ = "Copyright 2022 Greg Albrecht"
 __license__ = "Apache License, Version 2.0"
