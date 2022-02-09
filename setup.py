6a79c56ec600cbf31b5afaf2850d781ab374c6e0
Greg Albrecht <github.com@undef.net>
2022-02-09 15:43:39 -0800

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import sys
 import setuptools
 
 __title__ = "adsbxcot"
-__version__ = "1.6.0b1"
+__version__ = "1.6.0"
 __author__ = "Greg Albrecht W2GMD <oss@undef.net>"
 __copyright__ = "Copyright 2022 Greg Albrecht"
 __license__ = "Apache License, Version 2.0"
